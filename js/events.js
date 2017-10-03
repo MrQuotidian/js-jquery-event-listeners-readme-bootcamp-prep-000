f9bd63e37a97384326b0122fba54dba7d67caa25
Dan Daly <dan.john.daly@gmail.com>
2017-10-03 21:44:54 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -6,6 +6,26 @@ function getIt() {
   })
 }
 
+function frameIt() {
+  $( 'img' ).on( 'load', function() {
+    $(this).addClass('tasty')
+  })
+}
+
+function pressIt() {
+  // g = 71
+  $( 'input' ).on( 'keydown', function(c) {
+    if( parseInt(c,10) === 71 )
+      alert('G!')
+  })
+}
+
+function submitIt() {
+  $( 'form' ).on( 'submit', function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
 $( document ).ready( function() {
 
 // call functions here
